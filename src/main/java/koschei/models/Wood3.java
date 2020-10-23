c473989fc79d907db6136cf95f7757b2530fff79
f4nke <cool-vlad@mail.ru>
2020-10-23 11:47:08 +0300

    task-2.1.3

--- src/main/java/koschei/models/Wood3.java
+++ src/main/java/koschei/models/Wood3.java
@@ -1,6 +1,7 @@
 package koschei.models;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.stereotype.Component;
 
 @Component
